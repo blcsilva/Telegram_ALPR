1425602447688e8cb1dfc16d5c65c9ec4ec5f82e
Bruno Lucio <50206342+blcsilva@users.noreply.github.com>
2025-02-25 15:59:28 -0300

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ TELEGRAM_BOT_TOKEN = 'your-telegram-bot-token'
 TELEGRAM_CHAT_ID = 'admin-chat-id'  # ID do administrador para notificações
 
 # Token da API Plate Recognizer
-PLATE_RECOGNIZER_API_TOKEN = 'd12ff4fb3c3f258aa5f3efcf39d38c85920cc3bb'
+PLATE_RECOGNIZER_API_TOKEN = 'xxxxxxxxx'
